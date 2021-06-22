2eb6f1186528a46293668f11f2e013340a3dd551
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-22 20:25:25 +0000

    Added name

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-// Author:
+// Author: Jordan Muturi
 
 // Global UI Variables
-//let canvasDiv;
-
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
+ 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let video
+let isModelReady;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP();
+  textP.parent(textDiv);
+  video = createCapture("VIDEO");
+  video.style("display", "none");
+  isModelReady = false
+  mobilenet = ml5 imageClassifier("MobileNet", modelReady);
 }
 
 function draw() {
