5ea4899868b4218432254328ae16aaaa5d912cf3
Spryti <67284103+Spryti@users.noreply.github.com>
2021-06-22 20:41:20 +0000

    Finished Project 2-3

--- script.js
+++ script.js
@@ -16,22 +16,31 @@ function setup() {
   canvas = createCanvas(640, 480);
   canvas.parent(canvasDiv);
   textDiv = createDiv();
-  textP = createP();
+  textP = createP("Model loading, please wait...");
   textP.parent(textDiv);
-  video = createCapture("VIDEO");
+  video = createCapture(VIDEO);
   video.style("display", "none");
   isModelReady = false
-  mobilenet = ml5 imageClassifier("MobileNet", modelReady);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
 
 function draw() {
-
+  image(video, 0, 0);
+  if(isModelReady) {
+    mobilenet.classify(canvas, gotResults);
+}
 }
 
 function modelReady() {
-
+  isModelReady = true
 }
 
 function gotResults(error, results) {
-
+  if(error) {
+    console.error(error);
+  } else {
+    let label = results[0].label
+    let confidence = round(results[0].confidence, 2);
+    textP.html("Label: " + label + "   Confidence: " + confidence);
+  }
 }
